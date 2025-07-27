bbaddddf0ddcdf687e5ed331fb9d7e2680763b47
YarNix <05tranthanh310@gmail.com>
2025-07-27 17:59:17 +0700

    wip - added particle effect; code restructured

--- MikuMikuWorld/Application.cpp
+++ MikuMikuWorld/Application.cpp
@@ -301,7 +301,8 @@ namespace MikuMikuWorld
 		ResourceManager::loadTexture(texturesDir + "stage.png");
 		ResourceManager::loadTexture(texturesDir + "particles.png");
 
-		ResourceManager::loadTransform(texturesDir + "transform.txt");
+		ResourceManager::loadTransforms(texturesDir + "transform.txt");
+		ResourceManager::loadParticleEffects(texturesDir + "particles.pte");
 
 		// Cache note textures indices
 		noteTextures.notes = ResourceManager::getTexture(NOTES_TEX);
